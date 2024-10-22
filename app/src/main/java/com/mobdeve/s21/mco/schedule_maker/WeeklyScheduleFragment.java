fe72c4ec6c66c923f573efa7cebae3580578229c
OldMarkie <marc_tasarra@dlsu.edu.ph>
2024-10-22 12:08:48 +0800

    Updated Visual and Preparation For Weekly Schedule Overhaul

---
+++ app/src/main/java/com/mobdeve/s21/mco/schedule_maker/WeeklyScheduleFragment.java
@@ -0,0 +1,15 @@
+package com.mobdeve.s21.mco.schedule_maker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class WeeklyScheduleFragment extends Fragment {
+
+
+}
